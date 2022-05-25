d3693be0186378e8396f378ee1b56c5efde38681
Gigabyte <Gigabyte@DESKTOP-R540R4R>
2022-05-25 15:34:32 -0500

    Primer comiteo Thalo

--- CibertecProyectoFinal/src/menu/MenuPrincipal.java
+++ CibertecProyectoFinal/src/menu/MenuPrincipal.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MenuPrincipal extends JFrame {
 
 	private JPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu menuArchivo;
+	private JMenuItem menuSalir;
+	private JMenu menuMantenimiento;
+	private JMenuItem menuConsultar;
+	private JMenuItem menuModificarMaleta;
+	private JMenuItem menuListarMaleta;
 
 	/**
 	 * Launch the application.
@@ -34,6 +44,27 @@ public class MenuPrincipal extends JFrame {
 		setTitle("Tienda 1.0");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		menuArchivo = new JMenu("Archivo");
+		menuBar.add(menuArchivo);
+		
+		menuSalir = new JMenuItem("Salir");
+		menuArchivo.add(menuSalir);
+		
+		menuMantenimiento = new JMenu("Mantenimiento");
+		menuBar.add(menuMantenimiento);
+		
+		menuConsultar = new JMenuItem("Consultar Maletas");
+		menuMantenimiento.add(menuConsultar);
+		
+		menuModificarMaleta = new JMenuItem("Modificar Maletas");
+		menuMantenimiento.add(menuModificarMaleta);
+		
+		menuListarMaleta = new JMenuItem("Listar Maletas");
+		menuMantenimiento.add(menuListarMaleta);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
