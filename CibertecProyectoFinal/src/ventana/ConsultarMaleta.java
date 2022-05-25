039918f88914a85be4a26574548fcbad83cd3da9
Gigabyte <Gigabyte@DESKTOP-R540R4R>
2022-05-25 18:45:02 -0500

    ya está

--- CibertecProyectoFinal/src/ventana/ConsultarMaleta.java
+++ CibertecProyectoFinal/src/ventana/ConsultarMaleta.java
@@ -6,20 +6,25 @@ import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
+
+import menu.MenuPrincipal;
+
 import javax.swing.JButton;
 import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class ConsultarMaleta extends JInternalFrame {
+public class ConsultarMaleta extends JInternalFrame implements ActionListener {
 	private JLabel lblNewLabel;
 	private JLabel lblNewLabel_1;
-	private JComboBox<String> comboBox;
+	private JComboBox<String> cboTipoMaleta1;
 	private JLabel lblNewLabel_2;
 	private JLabel lblNewLabel_3;
 	private JLabel lblNewLabel_4;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtPrecio1;
+	private JTextField txtAncho1;
+	private JTextField txtAlto1;
+	private JTextField txtFondo1;
 	private JButton btnNewButton;
 
 	/**
@@ -46,58 +51,102 @@ public class ConsultarMaleta extends JInternalFrame {
 		setBounds(10, 10, 450, 300);
 		getContentPane().setLayout(null);
 		
-		lblNewLabel = new JLabel("New label");
+		lblNewLabel = new JLabel("Modelo");
 		lblNewLabel.setBounds(36, 50, 46, 14);
 		getContentPane().add(lblNewLabel);
 		
-		lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(36, 75, 46, 14);
+		lblNewLabel_1 = new JLabel("Precio (S/.)");
+		lblNewLabel_1.setBounds(36, 75, 59, 14);
 		getContentPane().add(lblNewLabel_1);
 		
-		comboBox = new JComboBox<String>();
-		comboBox.setModel(new DefaultComboBoxModel<String>(new String[] {"Aviator", "Century", "Benneton", "Lucas", "Samsonite"}));
-		comboBox.setBounds(128, 46, 85, 22);
-		getContentPane().add(comboBox);
+		cboTipoMaleta1 = new JComboBox<String>();
+		cboTipoMaleta1.addActionListener(this);
+		cboTipoMaleta1.setModel(new DefaultComboBoxModel<String>(new String[] {"Aviator", "Century", "Benneton", "Lucas", "Samsonite"}));
+		cboTipoMaleta1.setBounds(128, 46, 85, 22);
+		getContentPane().add(cboTipoMaleta1);
 		
-		lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(36, 100, 46, 14);
+		lblNewLabel_2 = new JLabel("Ancho (cm)");
+		lblNewLabel_2.setBounds(36, 100, 59, 14);
 		getContentPane().add(lblNewLabel_2);
 		
-		lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3 = new JLabel("Alto (cm)");
 		lblNewLabel_3.setBounds(36, 125, 46, 14);
 		getContentPane().add(lblNewLabel_3);
 		
-		lblNewLabel_4 = new JLabel("New label");
-		lblNewLabel_4.setBounds(36, 161, 46, 14);
+		lblNewLabel_4 = new JLabel("Fondo (cm)");
+		lblNewLabel_4.setBounds(36, 161, 59, 14);
 		getContentPane().add(lblNewLabel_4);
 		
-		textField = new JTextField();
-		textField.setEditable(false);
-		textField.setBounds(128, 72, 86, 20);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		txtPrecio1 = new JTextField();
+		txtPrecio1.setEditable(false);
+		txtPrecio1.setBounds(128, 72, 86, 20);
+		getContentPane().add(txtPrecio1);
+		txtPrecio1.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setBounds(127, 97, 86, 20);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		txtAncho1 = new JTextField();
+		txtAncho1.setEditable(false);
+		txtAncho1.setBounds(127, 97, 86, 20);
+		getContentPane().add(txtAncho1);
+		txtAncho1.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setEditable(false);
-		textField_2.setBounds(127, 122, 86, 20);
-		getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		txtAlto1 = new JTextField();
+		txtAlto1.setEditable(false);
+		txtAlto1.setBounds(127, 122, 86, 20);
+		getContentPane().add(txtAlto1);
+		txtAlto1.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setEditable(false);
-		textField_3.setBounds(128, 155, 86, 20);
-		getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		txtFondo1 = new JTextField();
+		txtFondo1.setEditable(false);
+		txtFondo1.setBounds(128, 155, 86, 20);
+		getContentPane().add(txtFondo1);
+		txtFondo1.setColumns(10);
 		
-		btnNewButton = new JButton("New button");
+		btnNewButton = new JButton("Cerrar");
 		btnNewButton.setBounds(262, 46, 89, 23);
 		getContentPane().add(btnNewButton);
+		
+		txtPrecio1.setText(MenuPrincipal.precio0+"");
+		txtAncho1.setText(MenuPrincipal.ancho0+"");
+		txtAlto1.setText(MenuPrincipal.alto0+"");
+		txtFondo1.setText(MenuPrincipal.fondo0+"");
 
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == cboTipoMaleta1) {
+			actionPerformedCboTipoMaleta1(e);
+		}
+	}
+	protected void actionPerformedCboTipoMaleta1(ActionEvent e) {
+		int index=cboTipoMaleta1.getSelectedIndex();
+			if(index==0) {
+				txtPrecio1.setText(MenuPrincipal.precio0+"");
+				txtAncho1.setText(MenuPrincipal.ancho0+"");
+				txtAlto1.setText(MenuPrincipal.alto0+"");
+				txtFondo1.setText(MenuPrincipal.fondo0+"");
+			}
+			if(index==1) {
+				txtPrecio1.setText(MenuPrincipal.precio1+"");
+				txtAncho1.setText(MenuPrincipal.ancho1+"");
+				txtAlto1.setText(MenuPrincipal.alto1+"");
+				txtFondo1.setText(MenuPrincipal.fondo1+"");
+			}
+			if(index==2) {
+				txtPrecio1.setText(MenuPrincipal.precio2+"");
+				txtAncho1.setText(MenuPrincipal.ancho2+"");
+				txtAlto1.setText(MenuPrincipal.alto2+"");
+				txtFondo1.setText(MenuPrincipal.fondo2+"");
+			}
+			if(index==3) {
+				txtPrecio1.setText(MenuPrincipal.precio3+"");
+				txtAncho1.setText(MenuPrincipal.ancho3+"");
+				txtAlto1.setText(MenuPrincipal.alto3+"");
+				txtFondo1.setText(MenuPrincipal.fondo3+"");
+			}
+			if(index==4) {
+				txtPrecio1.setText(MenuPrincipal.precio4+"");
+				txtAncho1.setText(MenuPrincipal.ancho4+"");
+				txtAlto1.setText(MenuPrincipal.alto4+"");
+				txtFondo1.setText(MenuPrincipal.fondo4+"");
+			}
 	}
 }
