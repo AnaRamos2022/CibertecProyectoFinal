ae0a79d59cbb74b737688f8d054ac28ce7741c8e
Gigabyte <Gigabyte@DESKTOP-R540R4R>
2022-06-04 10:49:20 -0500

    interface de ventas Thalo

--- CibertecProyectoFinal/src/ventana/GenerarReportes.java
+++ CibertecProyectoFinal/src/ventana/GenerarReportes.java
@@ -3,8 +3,23 @@ package ventana;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class GenerarReportes extends JInternalFrame {
+	private JDesktopPane desktopPane;
+	private JLabel lblNewLabel;
+	private JComboBox comboBox;
+	private JButton btnNewButton;
+	private JScrollPane scrollPane;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -26,7 +41,44 @@ public class GenerarReportes extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public GenerarReportes() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Generar reportes");
+		setBounds(100, 100, 649, 321);
+		
+		desktopPane = new JDesktopPane();
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(desktopPane, GroupLayout.DEFAULT_SIZE, 457, Short.MAX_VALUE)
+					.addGap(0))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(desktopPane, GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
+					.addGap(1))
+		);
+		
+		lblNewLabel = new JLabel("Tipo de reporte");
+		lblNewLabel.setBounds(10, 21, 90, 14);
+		desktopPane.add(lblNewLabel);
+		
+		comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Ventas por modelo", "Razón de precios en relación al precio mayor", "Estadística sobre la cantidad óptima", "Estadística sobre el precio"}));
+		comboBox.setBounds(110, 17, 284, 22);
+		desktopPane.add(comboBox);
+		
+		btnNewButton = new JButton("Cerrar");
+		btnNewButton.setBounds(524, 17, 89, 23);
+		desktopPane.add(btnNewButton);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 46, 603, 233);
+		desktopPane.add(scrollPane);
+		
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		getContentPane().setLayout(groupLayout);
 
 	}
 
--- CibertecProyectoFinal/src/ventana/Vender.java
+++ CibertecProyectoFinal/src/ventana/Vender.java
@@ -3,8 +3,28 @@ package ventana;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JDesktopPane;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class Vender extends JInternalFrame {
+	private JDesktopPane desktopPane;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JComboBox cboVender;
+	private JTextField txtFieldVender1;
+	private JTextField txtFieldVender2;
+	private JButton btnVender;
+	private JButton btnCerrar;
+	private JScrollPane scrollPane;
+	private JTextArea textArea;
 
 	/**
 	 * Launch the application.
@@ -26,8 +46,66 @@ public class Vender extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public Vender() {
+		setTitle("Vender");
 		setBounds(100, 100, 450, 300);
+		
+		desktopPane = new JDesktopPane();
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(desktopPane, GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
+					.addGap(0))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(desktopPane, GroupLayout.DEFAULT_SIZE, 279, Short.MAX_VALUE)
+					.addGap(1))
+		);
+		
+		lblNewLabel = new JLabel("Modelo");
+		lblNewLabel.setBounds(10, 24, 46, 14);
+		desktopPane.add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("Precio (S/.)");
+		lblNewLabel_1.setBounds(10, 49, 62, 14);
+		desktopPane.add(lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("Cantidad");
+		lblNewLabel_2.setBounds(10, 84, 46, 14);
+		desktopPane.add(lblNewLabel_2);
+		
+		cboVender = new JComboBox();
+		cboVender.setBounds(84, 20, 182, 22);
+		desktopPane.add(cboVender);
+		
+		txtFieldVender1 = new JTextField();
+		txtFieldVender1.setEditable(false);
+		txtFieldVender1.setBounds(82, 46, 184, 20);
+		desktopPane.add(txtFieldVender1);
+		txtFieldVender1.setColumns(10);
+		
+		txtFieldVender2 = new JTextField();
+		txtFieldVender2.setBounds(84, 81, 182, 20);
+		desktopPane.add(txtFieldVender2);
+		txtFieldVender2.setColumns(10);
+		
+		btnVender = new JButton("Vender");
+		btnVender.setBounds(310, 20, 89, 23);
+		desktopPane.add(btnVender);
+		
+		btnCerrar = new JButton("Cerrar");
+		btnCerrar.setBounds(310, 49, 89, 23);
+		desktopPane.add(btnCerrar);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 109, 414, 149);
+		desktopPane.add(scrollPane);
+		
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		getContentPane().setLayout(groupLayout);
 
 	}
-
 }
